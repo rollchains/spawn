3749311add135c4af7f3c65c6fabaf33c88bc64f
Reece Williams <reecepbcups@gmail.com>
2024-02-09 21:20:13 -0600

    get relative paths for local-ic + set ICTEST_HOME default

--- cmd/spawn/download-localic.go
+++ cmd/spawn/download-localic.go
@@ -21,13 +21,19 @@ var LocalICURL = "https://github.com/strangelove-ventures/interchaintest/release
 const (
 	FlagVersionOverride = "version"
 	FlagForceDownload   = "download"
+	FlagLocationPath    = "print-location"
 )
 
 func init() {
 	LocalICCmd.Flags().String(FlagVersionOverride, DefaultVersion, "change the local-ic version to use")
 	LocalICCmd.Flags().Bool(FlagForceDownload, false, "force download of local-ic")
+	LocalICCmd.Flags().Bool(FlagLocationPath, false, "print the location of local-ic binary")
 }
 
+// ---
+// make install && ICTEST_HOME=./simapp spawn local-ic start testnet
+// make install && cd simapp && spawn local-ic start testnet
+// ---
 var LocalICCmd = &cobra.Command{
 	Use:   "local-ic",
 	Short: "Local Interchain",
@@ -36,21 +42,48 @@ var LocalICCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		version, _ := cmd.Flags().GetString(FlagVersionOverride)
 		forceDownload, _ := cmd.Flags().GetBool(FlagForceDownload)
+		debugBinaryLoc, _ := cmd.Flags().GetBool(FlagLocationPath)
 
-		// TODO: maybe nest into an embed struct? or download to GOPATH automatically?
-		//
-		// make install && ICTEST_HOME=./simapp spawn local-ic start testnet
-		//
-		_, err := os.Stat("local-ic")
-		if forceDownload || err != nil {
+		loc := whereIsLocalICInstalled()
+		if (forceDownload || loc == "") && version != "" {
 			downloadBin(version)
+			loc = whereIsLocalICInstalled()
 		}
 
-		os.Chmod("./local-ic", 0755)
-		if err := execCommand("local-ic", args...); err != nil {
+		if debugBinaryLoc {
+			fmt.Println(loc)
+			return
+		}
+
+		if err := os.Chmod(loc, 0755); err != nil {
+			fmt.Println("Error setting local-ic permissions:", err)
+		}
+
+		// set to use the current dir if it is not overrriden
+		if os.Getenv("ICTEST_HOME") == "" {
+			if err := os.Setenv("ICTEST_HOME", "."); err != nil {
+				fmt.Println("Error setting ICTEST_HOME:", err)
+			}
+		}
+
+		if err := execCommand(loc, args...); err != nil {
 			fmt.Println("Error calling local-ic:", err)
 		}
 	},
+}
+
+func whereIsLocalICInstalled() string {
+	for _, path := range []string{"local-ic", "bin/local-ic", "local-interchain/localic"} {
+		if _, err := os.Stat(path); err == nil {
+			return path
+		}
+	}
+
+	if path, err := exec.LookPath("local-ic"); err == nil {
+		return path
+	}
+
+	return ""
 }
 
 func execCommand(command string, args ...string) error {
