99a3e90bd96314a22e76fbc84b5be712f3217051
Reece Williams <reecepbcups@gmail.com>
2024-02-20 20:43:55 -0600

    attempt to make go mod package a variable (untested)

--- simapp/scripts/protocgen.sh
+++ simapp/scripts/protocgen.sh
@@ -2,6 +2,9 @@
 
 set -e
 
+
+GO_MOD_PACKAGE="github.com/strangelove-ventures/simapp"
+
 echo "Generating gogo proto code"
 cd proto
 proto_dirs=$(find . -path -prune -o -name '*.proto' -print0 | xargs -0 -n1 dirname | sort | uniq)
@@ -10,7 +13,7 @@ for dir in $proto_dirs; do
     # this regex checks if a proto file has its go_package set to github.com/strangelove-ventures/poa/...
     # gogo proto files SHOULD ONLY be generated if this is false
     # we don't want gogo proto to run for proto files which are natively built for google.golang.org/protobuf
-    if grep -q "option go_package" "$file" && grep -H -o -c 'option go_package.*github.com/strangelove-ventures/simapp/api' "$file" | grep -q ':0$'; then
+    if grep -q "option go_package" "$file" && grep -H -o -c "option go_package.*$GO_MOD_PACKAGE/api" "$file" | grep -q ':0$'; then
       buf generate --template buf.gen.gogo.yaml $file
     fi
   done
@@ -21,7 +24,7 @@ buf generate --template buf.gen.pulsar.yaml
 
 cd ..
 
-cp -r github.com/strangelove-ventures/simapp/* ./
+cp -r $GO_MOD_PACKAGE/* ./
 rm -rf github.com
 
 # Copy files over for dep injection
