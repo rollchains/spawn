9e4fc9bf1a73cb30c24dc848a2422f0514b7780a
Reece Williams <reecepbcups@gmail.com>
2024-03-07 20:03:49 -0600

    add back helper methods

--- simapp/interchaintest/setup.go
+++ simapp/interchaintest/setup.go
@@ -1,10 +1,16 @@
 package e2e
 
 import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/cosmos/cosmos-sdk/crypto/keyring"
 	moduletestutil "github.com/cosmos/cosmos-sdk/types/module/testutil"
 	"github.com/strangelove-ventures/interchaintest/v8"
 	"github.com/strangelove-ventures/interchaintest/v8/chain/cosmos"
 	"github.com/strangelove-ventures/interchaintest/v8/ibc"
+	"github.com/strangelove-ventures/interchaintest/v8/testutil"
 
 	sdkmath "cosmossdk.io/math"
 	sdk "github.com/cosmos/cosmos-sdk/types"
@@ -90,3 +96,82 @@ var (
 	RelayerRepo    = "ghcr.io/cosmos/relayer"
 	RelayerVersion = "main"
 )
+
+// Other Helpers
+func ExecuteQuery(ctx context.Context, chain *cosmos.CosmosChain, cmd []string, i interface{}, extraFlags ...string) {
+	flags := []string{
+		"--node", chain.GetRPCAddress(),
+		"--output=json",
+	}
+	flags = append(flags, extraFlags...)
+
+	ExecuteExec(ctx, chain, cmd, i, flags...)
+}
+func ExecuteExec(ctx context.Context, chain *cosmos.CosmosChain, cmd []string, i interface{}, extraFlags ...string) {
+	command := []string{chain.Config().Bin}
+	command = append(command, cmd...)
+	command = append(command, extraFlags...)
+	fmt.Println(command)
+
+	stdout, _, err := chain.Exec(ctx, command, nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(string(stdout))
+	if err := json.Unmarshal(stdout, &i); err != nil {
+		fmt.Println(err)
+	}
+}
+
+// Executes a command from CommandBuilder
+func ExecuteTransaction(ctx context.Context, chain *cosmos.CosmosChain, cmd []string) (sdk.TxResponse, error) {
+	var err error
+	var stdout []byte
+
+	stdout, _, err = chain.Exec(ctx, cmd, nil)
+	if err != nil {
+		return sdk.TxResponse{}, err
+	}
+
+	if err := testutil.WaitForBlocks(ctx, 2, chain); err != nil {
+		return sdk.TxResponse{}, err
+	}
+
+	var res sdk.TxResponse
+	if err := json.Unmarshal(stdout, &res); err != nil {
+		return res, err
+	}
+
+	return res, err
+}
+
+func TxCommandBuilder(ctx context.Context, chain *cosmos.CosmosChain, cmd []string, fromUser string, extraFlags ...string) []string {
+	return TxCommandBuilderNode(ctx, chain.GetNode(), cmd, fromUser, extraFlags...)
+}
+
+func TxCommandBuilderNode(ctx context.Context, node *cosmos.ChainNode, cmd []string, fromUser string, extraFlags ...string) []string {
+	command := []string{node.Chain.Config().Bin}
+	command = append(command, cmd...)
+	command = append(command, "--node", node.Chain.GetRPCAddress())
+	command = append(command, "--home", node.HomeDir())
+	command = append(command, "--chain-id", node.Chain.Config().ChainID)
+	command = append(command, "--from", fromUser)
+	command = append(command, "--keyring-backend", keyring.BackendTest)
+	command = append(command, "--output=json")
+	command = append(command, "--yes")
+
+	gasFlag := false
+	for _, flag := range extraFlags {
+		if flag == "--gas" {
+			gasFlag = true
+		}
+	}
+
+	if !gasFlag {
+		command = append(command, "--gas", "500000")
+	}
+
+	command = append(command, extraFlags...)
+	return command
+}
