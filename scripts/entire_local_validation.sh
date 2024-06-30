621a904b2fbf0a7076747ce682203a966fa73d47
Reece Williams <reecepbcups@gmail.com>
2024-06-30 18:34:56 -0500

    further

--- scripts/entire_local_validation.sh
+++ scripts/entire_local_validation.sh
@@ -1,3 +1,6 @@
+#!/bin/bash
+set -m  ## set job control
+#
 # sh scripts/entire_local_validation.sh
 # Original:
 # This script takes what is normally done via github CI and runs it locally to ensure matrix variations work as expected
@@ -22,47 +25,80 @@ make install
 LOGS_DIR=${HOME}/spawnlogs
 mkdir -p $LOGS_DIR
 
+# docker network prune ??
+
 # TEst TODO:
   # create new module(s) + ibc middleware
+
+  # setup_network builds, waits, and runs the integration tests for a given network
+# this allows for multi-process execution in an enviroment via screen jobs
+function setup_network() {
+    name=$1
+
+    # Builds docker image, pushes to background. Starts installing binary
+    # Once completed, pops local-image back to foreground
+    #
+    # (cd $name; make local-image) & (cd $name; make install) && fg
+    screen -S $name-build -dm bash -c "(cd $name; make local-image) && (cd $name; make install)"
+    while screen -ls | grep -q $name-build; do
+        echo "Waiting for $name-build to finish"
+        sleep 3
+    done
+
+    ictests=$(find $name -type f -name "interchaintest-e2e.yml" -exec grep -o 'ictest-[a-zA-Z]*' {} \;) && echo $ictests
+
+    for test in $ictests; do
+        logFile=$logs/$test.log
+        screenName=$name-$test
+
+        echo "Running $screenName"
+        echo $screenName > $logFile
+        screen -S $screenName -dm bash -c "cd $name; make $test > $logFile 2>&1"
+    done
+}
 
 # create a new function base
 function base_standard_network() {
     name=spawntestbase
-    cd $name
 
-    logs=$LOGS_DIR/$name
-    mkdir -p $logs
+    logs=$LOGS_DIR/$name && mkdir -p $logs
 
-    # spawn new $name --bypass-prompt --bech32=abcd --bin=$(name)d --denom=uxyz --org=strangelove
+    spawn new $name --bypass-prompt --bech32=abcd --bin=$(echo name)d --denom=uxyz --org=strangelove
 
-    # Builds docker image, pushes to background. Starts installing binary
-    # Once completed, pops local-image back to foreground
-    # make local-image & make install && fg
-
-    ictests=$(find . -type f -name "interchaintest-e2e.yml" -exec grep -o 'ictest-[a-zA-Z]*' {} \;) && echo $ictests
-
-    for test in $ictests; do
-        logFile=$logs/$test.log
-        echo "" > $logFile
-        screenName=$name-$test
-        echo "Running $screenName"
-        screen -S $screenName -dm bash -c "make $test > $logFile 2>&1"
-    done
+    setup_network $name
 }
+
+function minimal_network() {
+    name=spawntestminimal
+
+    logs=$LOGS_DIR/$name && mkdir -p $logs
+
+    # Copy all from spawn new `--disable` list
+    spawn new $name --consensus=proof-of-stake --disable=tokenfactory,globalfee,ibc-packetforward,ibc-ratelimit,cosmwasm,wasm-light-client,optimistic-execution,ignite-cli --bech32=abcd --bin=$(echo $name)d  --denom=uxyz --org=strangelove
+
+    setup_network $name
+}
+
+
 
 function validate_all_test() {
     find $LOGS_DIR -type f -name "*.log" -exec grep -l "FAIL" {} \;
+    find $LOGS_DIR -type f -name "*.log" -exec grep -l "No rule to make target" {} \; # make file error
 }
 
-base_standard_network
+base_standard_network &
+minimal_network &
 
-# wait until there are no screens running
-while screen -ls | grep -q "[0-9]\."; do
-    echo "Waiting for screens to finish"
-    sleep 5
+# Wait for processes to complete
+while lines=$(screen -ls | grep  "[0-9]\." | wc -l) && [ $lines -gt 0 ]; do
+    echo "Waiting for $lines screens to finish"
+    sleep 3
 done
 
 echo "All screens finished, validating outputs"
 validate_all_test
 
-# rm go binaries named spawntest*d & docker images
+killall screen || true
+
+# Reminder here to rm go binaries named spawntest*d & docker imagesC
+# rm -rf spawntest*/
