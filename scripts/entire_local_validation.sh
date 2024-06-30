ee399c72f7829e14e3f24a7f83335921e90d6661
Reece Williams <reecepbcups@gmail.com>
2024-06-30 18:47:12 -0500

    backup

--- scripts/entire_local_validation.sh
+++ scripts/entire_local_validation.sh
@@ -15,6 +15,7 @@ set -m  ## set job control
 # - reference docs/TAGGED_RELEASE_CHECKLIST.md
 # - https://github.com/rollchains/spawn/issues/86
 # - scripts/bump_localic.sh
+# TODO: use scripts/matrix.sh generations here
 
 # The new version which spawn will be
 NEW_SPAWN_VERSION=v0.50.3
@@ -37,13 +38,13 @@ function setup_network() {
 
     # Builds docker image, pushes to background. Starts installing binary
     # Once completed, pops local-image back to foreground
-    #
     # (cd $name; make local-image) & (cd $name; make install) && fg
-    screen -S $name-build -dm bash -c "(cd $name; make local-image) && (cd $name; make install)"
-    while screen -ls | grep -q $name-build; do
-        echo "Waiting for $name-build to finish"
-        sleep 3
-    done
+
+    # screen -S $name-build -dm bash -c "(cd $name; make local-image) && (cd $name; make install)"
+    # while screen -ls | grep -q $name-build; do
+    #     echo "Waiting for $name-build to finish"
+    #     sleep 3
+    # done
 
     ictests=$(find $name -type f -name "interchaintest-e2e.yml" -exec grep -o 'ictest-[a-zA-Z]*' {} \;) && echo $ictests
 
@@ -57,28 +58,40 @@ function setup_network() {
     done
 }
 
-# create a new function base
-function base_standard_network() {
+function chain_base() {
     name=spawntestbase
-
     logs=$LOGS_DIR/$name && mkdir -p $logs
 
     spawn new $name --bypass-prompt --bech32=abcd --bin=$(echo name)d --denom=uxyz --org=strangelove
-
     setup_network $name
 }
 
-function minimal_network() {
+function chain_minimal() {
     name=spawntestminimal
-
     logs=$LOGS_DIR/$name && mkdir -p $logs
 
     # Copy all from spawn new `--disable` list
     spawn new $name --consensus=proof-of-stake --disable=tokenfactory,globalfee,ibc-packetforward,ibc-ratelimit,cosmwasm,wasm-light-client,optimistic-execution,ignite-cli --bech32=abcd --bin=$(echo $name)d  --denom=uxyz --org=strangelove
-
     setup_network $name
 }
 
+function chain_only_cosmwasm() {
+    name=spawntestcw
+    logs=$LOGS_DIR/$name && mkdir -p $logs
+
+    # Copy all from spawn new `--disable` list
+    spawn new $name  --disable=tokenfactory,globalfee,ibc-packetforward,ibc-ratelimit,wasm-light-client,optimistic-execution,ignite-cli --bech32=abcd --bin=aaaaad --denom=uxyz --org=reecepbcups
+    setup_network $name
+}
+
+function chain_only_wasmlightclient() {
+    name=spawntestcw
+    logs=$LOGS_DIR/$name && mkdir -p $logs
+
+    # Copy all from spawn new `--disable` list
+    spawn new $name --disable=tokenfactory,globalfee,ibc-packetforward,ibc-ratelimit,wasm-light-client,optimistic-execution,ignite-cli
+    setup_network $name
+}
 
 
 function validate_all_test() {
@@ -86,8 +99,11 @@ function validate_all_test() {
     find $LOGS_DIR -type f -name "*.log" -exec grep -l "No rule to make target" {} \; # make file error
 }
 
-base_standard_network &
-minimal_network &
+# run 1 after another (current docker network issue)
+chain_base
+chain_minimal
+# chain_only_cosmwasm
+# chain_only_wasmlightclient
 
 # Wait for processes to complete
 while lines=$(screen -ls | grep  "[0-9]\." | wc -l) && [ $lines -gt 0 ]; do
