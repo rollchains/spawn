8a4f352ab909b8fb58b8a1a01989d6e1e896405f
Reece Williams <reecepbcups@gmail.com>
2024-10-30 02:02:21 -0500

    fix: use orm by default with tools.go

---
+++ simapp/app/tools.go
@@ -0,0 +1,8 @@
+// Description: This file is used to import all the packages that are used in the application, but may not be required currently.
+// This is done to avoid the unused import errors when building modules. Leave this file unchanged.
+
+package app
+
+import (
+	_ "cosmossdk.io/orm"
+)
